ca0cef1711af972a2abd0e7d62189c9ab02cb346
Brady <34150846+bradybhalla@users.noreply.github.com>
2023-04-05 12:47:59 -0700

    does it work?

--- client/src/NameForm.tsx
+++ client/src/NameForm.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
 
 export default function NameForm() {
+  const [id, setId] = useState("");
   return (
     <Container className="mt-5 flex-fill">
       <form style={{ textAlign: "center" }}>
-        <input type="hidden" name="id" value={Math.floor(Math.random() * 1e10)} />
+        {/*USE RANDOM ID: <input type="hidden" name="id" value={Math.floor(Math.random() * 1e10)} />*/}
+        <input type="hidden" name="id" value={id} />
         <div className="input-group">
-          <input name="name" autoComplete="off" className="bg-white form-control" placeholder="Enter your name..." required />
+          <input name="name" autoComplete="off" className="bg-white form-control" placeholder="Enter your name..." onChange={(e) => {
+            setId(e.target.value);
+          }} required />
           <button type="submit" className="btn btn-primary">Go</button>
         </div>
       </form>
